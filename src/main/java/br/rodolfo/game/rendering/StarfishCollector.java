43115816d3113f52d19f33d922d46b206ba87b02
Joao Rodolfo <joaorodolforosa@gmail.com>
2025-04-17 14:49:10 -0300

    Adicionada a anotação override e corrigida renderização da estrela

--- src/main/java/br/rodolfo/game/rendering/StarfishCollector.java
+++ src/main/java/br/rodolfo/game/rendering/StarfishCollector.java
@@ -10,72 +10,80 @@ import com.badlogic.gdx.math.Rectangle;
 
 public class StarfishCollector extends Game {
 
-	private SpriteBatch batch;
+    private SpriteBatch batch;
 
-	private Texture turtleTexture;
-	private float turtleX;
-	private float turtleY;
-	private Rectangle turtleRectangle;
+    private Texture turtleTexture;
+    private float turtleX;
+    private float turtleY;
+    private Rectangle turtleRectangle;
 
-	private Texture starfishTexture;
-	private float starfishX;
-	private float starfishY;
-	private Rectangle starfishRectangle;
+    private Texture starfishTexture;
+    private float starfishX;
+    private float starfishY;
+    private Rectangle starfishRectangle;
 
-	private Texture oceanTexture;
-	private Texture winMessageTexture;
+    private Texture oceanTexture;
+    private Texture winMessageTexture;
 
-	private boolean win;
+    private boolean win;
 
-	@Override
-	public void create() {
-		batch = new SpriteBatch();
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
 
-		turtleTexture = new Texture(Gdx.files.internal("images/turtle-1.png"));
-		turtleX = 20;
-		turtleY = 20;
-		turtleRectangle = new Rectangle(turtleX, turtleY, turtleTexture.getWidth(),
-				turtleTexture.getHeight());
+        turtleTexture = new Texture(Gdx.files.internal("images/turtle-1.png"));
+        turtleX = 20;
+        turtleY = 20;
+        turtleRectangle = new Rectangle(turtleX, turtleY, turtleTexture.getWidth(),
+                turtleTexture.getHeight());
 
-		starfishTexture = new Texture(Gdx.files.internal("images/starfish.png"));
-		starfishX = 380;
-		starfishY = 380;
-		starfishRectangle = new Rectangle(starfishX, starfishY, starfishTexture.getWidth(),
-				starfishTexture.getHeight());
+        starfishTexture = new Texture(Gdx.files.internal("images/starfish.png"));
+        starfishX = 380;
+        starfishY = 380;
+        starfishRectangle = new Rectangle(starfishX, starfishY, starfishTexture.getWidth(),
+                starfishTexture.getHeight());
 
-		oceanTexture = new Texture(Gdx.files.internal("images/water.jpg"));
-		winMessageTexture = new Texture(Gdx.files.internal("images/you-win.png"));
+        oceanTexture = new Texture(Gdx.files.internal("images/water.jpg"));
+        winMessageTexture = new Texture(Gdx.files.internal("images/you-win.png"));
 
-		win = false;
+        win = false;
 
-	}
+    }
 
-	public void render() {
-		if (Gdx.input.isKeyPressed(Keys.LEFT))
-			turtleX--;
-		if (Gdx.input.isKeyPressed(Keys.RIGHT))
-			turtleX++;
-		if (Gdx.input.isKeyPressed(Keys.UP))
-			turtleY++;
-		if (Gdx.input.isKeyPressed(Keys.DOWN))
-			turtleY--;
+    @Override
+    public void render() {
+        if (Gdx.input.isKeyPressed(Keys.LEFT)) {
+            turtleX--;
+        }
+        if (Gdx.input.isKeyPressed(Keys.RIGHT)) {
+            turtleX++;
+        }
+        if (Gdx.input.isKeyPressed(Keys.UP)) {
+            turtleY++;
+        }
+        if (Gdx.input.isKeyPressed(Keys.DOWN)) {
+            turtleY--;
+        }
 
-		turtleRectangle.setCenter(turtleX, turtleY);
+        turtleRectangle.setPosition(turtleX, turtleY);
 
-		if (turtleRectangle.overlaps(starfishRectangle))
-			win = true;
+        if (turtleRectangle.overlaps(starfishRectangle)) {
+            win = true;
+        }
 
-		Gdx.gl.glClearColor(0, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-		batch.begin();
-		batch.draw(oceanTexture, 0, 0);
-		if (win)
-			batch.draw(starfishTexture, starfishX, starfishY);
-		batch.draw(turtleTexture, turtleX, turtleY);
-		if (win)
-			batch.draw(winMessageTexture, 180, 180);
-		batch.end();
-	}
+        batch.begin();
+        batch.draw(oceanTexture, 0, 0);
+        if (!win) {
+            batch.draw(starfishTexture, starfishX, starfishY);
+        }
+        batch.draw(turtleTexture, turtleX, turtleY);
+        if (win) {
+            batch.draw(winMessageTexture, 180, 180);
+        }
+        batch.end();
+    }
 
 }
